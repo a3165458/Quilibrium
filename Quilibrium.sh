c7bf77b3fa3e934606b569741850e70f9aae01ba
smeb y <48400087+a3165458@users.noreply.github.com>
2024-03-02 14:34:47 +0800

    Update Quilibrium.sh

--- Quilibrium.sh
+++ Quilibrium.sh
@@ -49,20 +49,18 @@ cd ceremonyclient/node
 # 写入脚本
 cat > auto.sh <<EOF
 #!/bin/bash
-while true; do
-    # 检查node进程是否存在
-    ps -ef | grep "node" | grep -v "grep"
-    if [ "$?" -eq 1 ]; then
-        # 如果node进程不存在，输出信息并启动node
-        echo "Node进程未运行，正在尝试重新启动..."
-        GOEXPERIMENT=arenas go run ./...
-        echo "Node进程已启动。"
-    else
-        # 如果node进程正在运行，输出信息
-        echo "Node进程已经在运行中。"
-    fi
-    # 每次检查后休眠10秒
-    sleep 10
+while true
+do
+	ps -ef | grep "node" | grep -v "grep"
+	if [ "$?" -eq 1 ]
+		then 
+		echo "process has restarting..."
+		GOEXPERIMENT=arenas go run ./...
+		echo "process has been restarted!"
+	else
+		echo "process already started!"
+	fi
+	sleep 10
 done
 EOF
 
