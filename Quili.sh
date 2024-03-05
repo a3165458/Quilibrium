10c5dd6a9baa588e7700d6d8e5412cdeb2d08e49
smeb y <48400087+a3165458@users.noreply.github.com>
2024-03-05 14:46:29 +0800

    Update Quili.sh

--- Quili.sh
+++ Quili.sh
@@ -62,7 +62,7 @@ echo "sysctl配置已重新加载"
 sudo apt update && sudo apt -y upgrade 
 
 # 安装wget、screen和git等组件
-sudo apt install git ufw bison screen binutils gcc make -y
+sudo apt install git ufw bison screen binutils gcc make bsdmainutils -y
 
 # 安装GVM
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
@@ -126,7 +126,7 @@ echo "sysctl配置已重新加载"
 sudo apt update && sudo apt -y upgrade 
 
 # 安装wget、screen和git等组件
-sudo apt install git ufw bison screen binutils gcc make -y
+sudo apt install git ufw bison screen binutils gcc make bsdmainutils -y
 
 # 安装GVM
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
