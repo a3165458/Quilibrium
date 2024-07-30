9a761a442fc15dc7097b3d6c45654f5c558ef19b
smeb y <48400087+a3165458@users.noreply.github.com>
2024-07-30 17:21:38 +0800

    Update Quili.sh

--- Quili.sh
+++ Quili.sh
@@ -228,7 +228,7 @@ echo "=======================备份完成，请执行cd ~/backup 查看备份文
 
 function check_balance() {
 cd ~/ceremonyclient/node
-version="1.4.21"
+version="1.4.21.1"
 binary="node-$version"
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     if [[ $(uname -m) == "aarch64"* ]]; then
